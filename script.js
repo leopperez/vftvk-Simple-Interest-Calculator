67ebb94a39f3ba2d3cf255ec6d48e0548106426f
leopperez <55115351+leopperez@users.noreply.github.com>
2022-03-15 20:11:36 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
 function computeInterest()
 {
      var principal = document.getElementById("principal").value;
 
+     //Check if principal  is 0 or negative
     if(principal < 1){
         alert("Please enter a positive value");
         document.getElementById("principal").focus();
@@ -10,9 +10,11 @@ function computeInterest()
     }
      var rate = document.getElementById("rate").value;
      var years = document.getElementById("years").value;
+     //Calculate Interest
      var interest = principal * years * rate /100;
      var year = new Date().getFullYear()+parseInt(years) ;
      var amount = principal + interest
+     //show the result
      document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 
 }
@@ -21,4 +23,4 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval +"%";
-}
+} 
